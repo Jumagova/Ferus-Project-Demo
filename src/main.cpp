682c01e913f88827a0469b0fa078314fc5a55829
Juan Gomez <jumgomezva@unal.edu.co>
2023-04-20 18:32:18 -0500

    Screen integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,131 @@
+
+
 #include <Arduino.h>
+#include <screen_config.cpp>
+#include <lvgl.h>
+
+// #include <TFT_eSPI.h>
+#include "ui.h"
 
 #define DIR 21
 #define PULL 14
 
-void runMotor(int direction);
+LGFX tft;
+
+/*If you want to use the LVGL examples,
+  make sure to install the lv_examples Arduino library
+  and uncomment the following line.
+#include <lv_examples.h>
+*/
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth = 480;
+static const uint16_t screenHeight = 320;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * screenHeight / 10];
+
+// TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char *buf)
+{
+  Serial.printf(buf);
+  Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+  tft.endWrite();
+
+  lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  uint16_t touchX = 0, touchY = 0;
+
+  // bool touched = false; // tft.getTouch( &touchX, &touchY, 600 );
+  bool touched = tft.getTouch(&touchX, &touchY, 600);
+
+  if (!touched)
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    /*Set the coordinates*/
+    data->point.x = touchX;
+    data->point.y = touchY;
+
+    Serial.print("Data x ");
+    Serial.println(touchX);
+
+    Serial.print("Data y ");
+    Serial.println(touchY);
+  }
+}
 
 void setup()
 {
+  Serial.begin(115200); /* prepare for possible serial debug */
+
+  String LVGL_Arduino = "Hello Arduino! ";
+  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+  Serial.println(LVGL_Arduino);
+  Serial.println("I am LVGL_Arduino");
+
   pinMode(PULL, OUTPUT); // set PULL as PWM output
   pinMode(DIR, OUTPUT);  // set DIR as Direction
+
+  lv_init();
+
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print); /* register print function for debugging */
+#endif
+
+  tft.begin();        /* TFT init */
+  tft.setRotation(3); /* Landscape orientation, flipped */
+
+  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10);
+
+  /*Initialize the display*/
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  /*Change the following line to your display resolution*/
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  /*Initialize the (dummy) input device driver*/
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  ui_init();
+
+  Serial.println("Setup done");
 }
 
 void loop()
 {
-  runMotor(HIGH); // run the motor in the clockwise direction at 50 RPM
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
 }
-
-void runMotor(int direction)
-{
-
-  int x;
-  unsigned long currentMillis = 0;
-  const unsigned long period = 1; // set period to 5ms
-  int dutyCycle = 0;              // initial duty cycle value
-
-  // int period = 60000 / speed / 400; // calculate the period in milliseconds
-  digitalWrite(DIR, direction); // set the direction
-  for (int x = 0; x < 400; x++)
-  { // repeat 400 times a revolution when setting 400 on driver
-    unsigned long currentMillis = millis();
-    if (currentMillis % period == 0)
-    {                                             // if the period has passed
-      int dutyCycle = (dutyCycle == 0) ? 255 : 0; // toggle the duty cycle between 0 and 255
-      analogWrite(PULL, dutyCycle);               // set the duty cycle
-    }
-  }
-}
-
-

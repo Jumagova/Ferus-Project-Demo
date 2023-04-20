7c1bb7ff5886ddef7dfed8ab4c4c963b10c40326
Juan Gomez <jumgomezva@unal.edu.co>
2023-04-20 12:06:25 -0500

    Corrected function for motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,40 @@
-#include<Arduino.h>
+#include <Arduino.h>
 
-int x;
-unsigned long currentMillis = 0;
-const unsigned long period = 5; // set period to 5ms
-int dutyCycle = 0; // initial duty cycle value
+#define DIR 21
+#define PULL 14
 
-void runMotor(int direction, int speed);
+void runMotor(int direction);
 
-void setup() {
-  pinMode(14, OUTPUT); // set Pin14 as PWM output
-  pinMode(21, OUTPUT); // set Pin21 as DIR
+void setup()
+{
+  pinMode(PULL, OUTPUT); // set PULL as PWM output
+  pinMode(DIR, OUTPUT);  // set DIR as Direction
 }
 
-void loop() {
-  runMotor(HIGH, 200); // run the motor in the clockwise direction at 50 RPM
+void loop()
+{
+  runMotor(HIGH); // run the motor in the clockwise direction at 50 RPM
 }
 
-void runMotor(int direction, int speed) {
-  int period = 60000 / speed / 400; // calculate the period in milliseconds
-  digitalWrite(21, direction); // set the direction
-  for (int x = 0; x < 400; x++) { // repeat 400 times a revolution when setting 400 on driver
+void runMotor(int direction)
+{
+
+  int x;
+  unsigned long currentMillis = 0;
+  const unsigned long period = 1; // set period to 5ms
+  int dutyCycle = 0;              // initial duty cycle value
+
+  // int period = 60000 / speed / 400; // calculate the period in milliseconds
+  digitalWrite(DIR, direction); // set the direction
+  for (int x = 0; x < 400; x++)
+  { // repeat 400 times a revolution when setting 400 on driver
     unsigned long currentMillis = millis();
-    if (currentMillis % period == 0) { // if the period has passed
+    if (currentMillis % period == 0)
+    {                                             // if the period has passed
       int dutyCycle = (dutyCycle == 0) ? 255 : 0; // toggle the duty cycle between 0 and 255
-      analogWrite(14, dutyCycle); // set the duty cycle
+      analogWrite(PULL, dutyCycle);               // set the duty cycle
     }
   }
 }
+
+
